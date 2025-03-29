fa6c7ef871d445c259065b26013f601a11c76fef
Luka <luka07765@gmail.com>
2025-03-29 18:46:04 +0100

    mapu radio

---
+++ src/app/notebook/sidebar-tree/page.jsx
@@ -0,0 +1,37 @@
+'use client';
+import { useFolderListLogic } from '@/Server/Apollo/Logic/SideBar/QuerySideBar';
+import SidebarTree from '@/Components/SidebarTree/Master_Tree';
+import { buildNestedStructure } from '@/Utils/Data_Structure/Structure';
+function Page() {
+  const { folders, error, loading } = useFolderListLogic();
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <p className="text-gray-500">Loading folders...</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <p className="text-red-500">Error loading folders: {error.message}</p>
+      </div>
+    );
+  }
+  const nestedFolders = buildNestedStructure(folders);
+
+  console.log(JSON.stringify(nestedFolders, null, 2));
+
+  return (
+    <div>
+      {nestedFolders ? (
+        <SidebarTree structure={nestedFolders} />
+      ) : (
+        <p className="text-gray-500">No folders to display.</p>
+      )}
+    </div>
+  );
+}
+
+export default Page;
