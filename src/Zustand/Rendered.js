b7d7497bae96c80a3e7ea15bac7b3fb4a5895826
Luka <luka07765@gmail.com>
2025-06-05 01:28:41 -0700

    sdfe

--- src/Zustand/Rendered.js
+++
@@ -1,9 +0,0 @@
-import { create } from 'zustand';
-
-export const Render = create((set) => ({
-  folder: [],
-  setFolder: (newFolders) => set({ folders: newFolders }),
-
-  file: [],
-  setFile: (newFolders) => set({ folders: newFolders }),
-}));
