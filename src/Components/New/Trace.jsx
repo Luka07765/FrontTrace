e8b699b2e90989a1a24436a3d91e55ec589da710
unknown <luka07765@gmail.com>
2025-09-28 21:48:57 +0200

    3.2

--- src/Components/New/Trace.jsx
+++ src/Components/New/Trace.jsx
@@ -2,22 +2,28 @@
 import Folder_Data from '@/Components/New/Data/Folder_Data';
 import Folder from '@/Components/New/Render/Folder_Render';
 import { motion, AnimatePresence } from 'framer-motion';
-import { useState} from "react";
+import { useState,useRef} from "react";
 import NextIcon from "@/assets/FolderFile_Icons/next.png"
 import Image from 'next/image';
+import "@/Components/New/Render/style.css"
 export default function Trace() {
     const [expand, setExpand] = useState(false);
+
+
   return (
     <motion.div className="relative flex h-screen overflow-hidden">
+      <motion.div   
 
-      <motion.div className="overflow-y-auto overflow-x-hidden h-full bg-gray-900 text-white items-left pb-40 "
-      animate={{ maxWidth: expand ? '7rem' : '16rem' }}
+        className="sidebar overflow-x-hidden h-full bg-gray-900 text-white pb-40"
+      animate={{ maxWidth: expand ? '6rem' : '16rem' }}
   transition={{ type: 'spring', damping: 15 }}
         >
 
-   
+       
         <Folder_Data render={folder => (
-          <li className="list-none" key={folder.id}>
+          
+          <li
+           className="list-none" key={folder.id}>
             <Folder folder={folder}  />
           </li>
         )}/>
