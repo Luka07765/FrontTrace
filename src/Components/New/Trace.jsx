c661ca85962585775114853e24b8c4c296271a03
unknown <luka07765@gmail.com>
2025-09-28 22:40:48 +0200

    3.5

--- src/Components/New/Trace.jsx
+++ src/Components/New/Trace.jsx
@@ -8,39 +8,31 @@ import Image from 'next/image';
 import "@/Components/New/Render/style.css"
 export default function Trace() {
     const [expand, setExpand] = useState(false);
-
-
   return (
     <motion.div className="relative flex h-screen overflow-hidden">
       <motion.div   
-
-        className="sidebar overflow-x-hidden h-full bg-gray-900 text-white pb-40"
-      animate={{ maxWidth: expand ? '6rem' : '16rem' }}
-  transition={{ type: 'spring', damping: 15 }}
+          className="sidebar overflow-x-hidden h-full bg-gray-900 text-white pb-40"
+          animate={{ maxWidth: expand ? '6rem' : '16rem' }}
+          transition={{ type: 'spring', damping: 15 }}
         >
-
-       
-        <Folder_Data render={folder => (
-          
-          <li
-           className="list-none" key={folder.id}>
-            <Folder folder={folder}  />
-          </li>
-        )}/>
+          <Folder_Data render={folder => (
+            <li className="list-none" key={folder.id}>
+                <Folder folder={folder}  />
+            </li> )}/>
       </motion.div>
-<button
-  onClick={() => setExpand(!expand)}
-  className="h-8 w-8 flex items-center justify-center opacity-0 hover:opacity-100 transition-all duration-200"
->
+      
+      <button
+        onClick={() => setExpand(!expand)}
+        className="h-8 w-8 flex items-center justify-center opacity-0 hover:opacity-100 transition-all duration-200"
+      >
       <Image
-                  src={ NextIcon }
-                  alt={'Folder Open' }
-                  width={35}
-                  height={25}
-                 className="filter invert"
-                />
+          src={ NextIcon }
+          alt={'Folder Open' }
+          width={35}
+          height={25}
+          className="filter invert"
+      />
 </button>
-
     </motion.div>
   );
 }
