b9d2ea7ba2b3771435785f7c302bdab147e2d52d
unknown <luka07765@gmail.com>
2025-09-28 17:48:11 +0200

    2.3

--- src/Components/New/Render/Folder_Render.jsx
+++ src/Components/New/Render/Folder_Render.jsx
@@ -6,20 +6,20 @@ import folderOpenIcon from '@/assets/FolderFile_Icons/open-folder.png';
 function Folder_Render({ folder }) {
   return (
     <motion.div
-      className="flex items-center justify-between p-3 rounded-lg cursor-pointer hover:bg-gray-700 transition-colors duration-200"
+      className="flex items-center  pr-6 ml-2 justify-between p-3 rounded-lg cursor-pointer hover:bg-gray-700 transition-colors duration-200 group"
       whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.98 }}
     >
       {/* Left side: icon + title */}
-      <div className="flex items-center space-x-3 ">
+      <div className="flex items-center space-x-3 overflow-hidden">
         <Image
           src={folderOpenIcon}
           alt="Folder Open"
           width={40}
           height={40}
-          className="filter invert"
+            className="filter invert flex-shrink-0"
         />
-        <span className="text-white  font-medium text-base truncate">{folder.title}</span>
+        <span className="text-white  font-medium text-base truncate group-hover:whitespace-normal group-hover:overflow-visible">{folder.title}</span>
       </div>
 
       {/* Right side: grab handle */}
--- src/Components/New/Trace.jsx
+++ src/Components/New/Trace.jsx
@@ -5,7 +5,7 @@ import Folder from '@/Components/New/Render/Folder_Render';
 export default function Trace() {
   return (
     <div className="relative flex h-screen overflow-hidden">
-      <div className="w-60 overflow-y-auto overflow-x-hidden h-full bg-gray-900 text-white items-left py-4">
+      <div className="w-60 overflow-y-auto overflow-x-hidden h-full bg-gray-900 text-white items-left pb-40 ">
         <Folder_Data render={folder => (
           <li className="list-none" key={folder.id}>
             <Folder folder={folder} />
